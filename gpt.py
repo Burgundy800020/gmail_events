bfac9928d53ea4f8f3c41ad967adb3f543ba9183
Yizhou <yizhou.wang.1218@gmail.com>
2025-06-19 16:42:02 -0400

    fixed timezone lag

--- gpt.py
+++ gpt.py
@@ -1,8 +1,8 @@
-from datetime import datetime
+from datetime import datetime as dt
 from typing import List
 
 from openai import OpenAI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from config import settings
 from structlog import get_logger
 logger = get_logger(__name__)
@@ -12,20 +12,26 @@ def init_openai():
 
 class Event(BaseModel):
     name: str
-    datetime: datetime
+    datetime: dt
     location: str
     items_to_bring: list[str]
 
-PROMPT = ("You will be given an email body. Extract the event information. "
-          "If no event or a field is missing, leave it blank. "
-          f"Today's date is {datetime.now().strftime('%A, %B %d, %Y')} "
-          f"and the current time is {datetime.now().strftime('%I:%M %p')}."
-          )
 
 def get_events(client: OpenAI, messages) -> List[Event]:
+    if not messages: return []
+    local_time = dt.now().astimezone()
+    PROMPT = ("You will be given an email body. Extract the event information. "
+              "If no event or a field is missing, leave it blank. "
+              f"Today's date is {local_time.strftime('%A, %B %d, %Y')} "
+              f"and the current local time is {local_time.strftime('%I:%M %p')}. "
+              f"The timezone of my region is {local_time.tzinfo}, so output "
+              f"all time-related information in {local_time.tzinfo}. "
+              )
     responses = []
+    logger.info("Prompting gpt", prompt=PROMPT)
     for message in messages:
         try:
+            logger.info("gpt input time", time=dt.now().strftime('%I:%M %p'))
             response = client.responses.parse(
                 model=settings.GPT_MODEL,
                 input=[
@@ -34,7 +40,10 @@ def get_events(client: OpenAI, messages) -> List[Event]:
                 ],
                 text_format=Event
             )
-            responses.append(response.output_parsed)
+            extracted_event:Event = response.output_parsed
+            logger.info("gpt output time", time=extracted_event.datetime.strftime('%I:%M %p'))
+            logger.info("gpt output tz", tz=extracted_event.datetime.tzinfo)
+            responses.append(extracted_event)
         except Exception as e:
             logger.error("event_extraction_failed", error=str(e), message=message[:100])
             continue
